e55cc4d60740eef6824ec3004431044829267b8d
Ian Rodrigues <iansrodrigs@gmail.com>
2018-02-25 14:34:59 -0300

    wip: login form

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,14 @@
 |
 */
 
+Route::get('/login', function () {
+    return view('login');
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+// Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home');
